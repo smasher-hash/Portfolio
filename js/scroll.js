4caa6a85d691a96c5c77cb506b6afa1cccbd60ad
mushkan pandey <mushkanpandey501@gmail.com>
2026-06-16 21:57:45 +0530

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+window.addEventListener(
+
+"scroll",
+
+function(){
+
+const navbar =
+document.querySelector(".navbar");
+
+navbar.classList.toggle(
+"active",
+window.scrollY > 50
+);
+
+}
+
+);
---
+++ js/typing.js
@@ -0,0 +1,24 @@
+var typed = new Typed("#typing", {
+
+strings:[
+
+"MERN Stack Developer",
+
+"Frontend Developer",
+
+"Web Designer",
+
+"Problem Solver",
+
+"Quick Learner",
+
+"Adaptable"
+
+
+],
+
+typeSpeed:80,
+backSpeed:50,
+loop:true
+
+});
